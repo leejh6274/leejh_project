332c5dd42bab39e8558be8cd6b944b93af245553
leejh6274 <thsou36@gmail.com>
2023-04-14 17:18:12 +0900

    마이바티스, MySql JDBC 드라이버 추가, 설정 파일에 DB접속정보 기입

---
+++ src/main/java/com/ljh/exam/demo/repository/ArticleRepository.java
@@ -0,0 +1,28 @@
+package com.ljh.exam.demo.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ljh.exam.demo.vo.Article;
+
+@Mapper
+public interface ArticleRepository {
+  
+ 
+   public Article getArticle(int id);
+    
+   
+   public List<Article> getArticles();
+  
+   
+   public Article writeArticle(String title, String body);
+   
+   
+   public void deleteArticle(int id);
+
+   
+   public void modifyArticle(int id, String title, String body);
+    
+
+}
--- src/main/java/com/ljh/exam/demo/service/ArticleService.java
+++ src/main/java/com/ljh/exam/demo/service/ArticleService.java
@@ -1,8 +1,38 @@
 package com.ljh.exam.demo.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.ljh.exam.demo.repository.ArticleRepository;
+import com.ljh.exam.demo.vo.Article;
 
 @Service
 public class ArticleService {
+   private ArticleRepository articleRepository;
+   
+   public ArticleService() {
+      this.articleRepository = articleRepository;
+      
+   }
+   
+   public Article getArticle(int id) {
+      return articleRepository.getArticle(id);
+   }
+   
+   public List<Article> getArticles() {
+      return articleRepository.getArticles();
+   }
+   
+   public Article writeArticle(String title, String body) {
+      return articleRepository.writeArticle(title, body);
+   }
+   
+   public void deleteArticle(int id) {
+      articleRepository.deleteArticle(id);
+   }
 
+   public void modifyArticle(int id, String title, String body) {
+      articleRepository.modifyArticle(id, title, body);
+   }
 }
