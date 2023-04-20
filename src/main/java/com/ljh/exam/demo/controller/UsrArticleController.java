72e4518f632ecc2ea5b664941b1a6909228fd91d
leejh6274 <thsou36@gmail.com>
2023-04-20 17:48:41 +0900

    Rq 객체를 BeforeActionInterceptor에서대표를 한번만 만들기

--- src/main/java/com/ljh/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/ljh/exam/demo/controller/UsrArticleController.java
@@ -26,7 +26,7 @@ public class UsrArticleController {
    @RequestMapping("/usr/article/doAdd")
    @ResponseBody
    public ResultData<Article> doAdd(HttpServletRequest req, String title, String body) {
-      Rq rq = new Rq(req);
+      Rq rq = (Rq)req.getAttribute("rq");
 
       if (rq.isLogined() == false) {
          return ResultData.from("F-A", "로그인 후 이용해주세요.");
@@ -54,7 +54,7 @@ public class UsrArticleController {
    @RequestMapping("/usr/article/list")
    public String showList(HttpServletRequest req, Model model) {
      
-	  Rq rq = new Rq(req);
+	   Rq rq = (Rq)req.getAttribute("rq");
 
       List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId());
       model.addAttribute("articles", articles);
@@ -63,7 +63,7 @@ public class UsrArticleController {
 
    @RequestMapping("/usr/article/detail")
    public String showDetail(HttpServletRequest req, Model model, int id) {
-	  Rq rq = new Rq(req);
+	   Rq rq = (Rq)req.getAttribute("rq");
 
       Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
 
@@ -76,7 +76,7 @@ public class UsrArticleController {
    @ResponseBody
    public ResultData<Article> getArticle(HttpServletRequest req, int id) {
      
-	  Rq rq = new Rq(req);
+	   Rq rq = (Rq)req.getAttribute("rq");
 	   
       Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
 
@@ -91,7 +91,7 @@ public class UsrArticleController {
    @ResponseBody
    public String doDelete(HttpServletRequest req, int id) {
       
-	  Rq rq = new Rq(req);
+	   Rq rq = (Rq)req.getAttribute("rq");
 
       if (rq.isLogined() == false) {
          return Ut.jsHistoryBack("로그인 후 이용해주세요.");
@@ -99,14 +99,15 @@ public class UsrArticleController {
 
       Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
 
-      if (article.getMemberId() != rq.getLoginedMemberId()) {
-         return Ut.jsHistoryBack("삭제 권한이 없습니다.");
-      }
 
       if (article == null) {
          ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
       }
 
+      if (article.getMemberId() != rq.getLoginedMemberId()) {
+    	  return Ut.jsHistoryBack("삭제 권한이 없습니다.");
+      }
+      
       articleService.deleteArticle(id);
 
       return Ut.jsReplace(Ut.f("%d번 게시물을 삭제하였습니다.", id), "../article/list");
@@ -116,7 +117,7 @@ public class UsrArticleController {
    @ResponseBody
    public ResultData<Article> doModify(HttpServletRequest req, int id, String title, String body) {
       
-	   Rq rq = new Rq(req);
+	   Rq rq = (Rq)req.getAttribute("rq");
 
       if (rq.isLogined() == false) {
          return ResultData.from("F-A", "로그인 후 이용해주세요.");
