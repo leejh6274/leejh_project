62f96ebc7f901ae7702942cd739b61c7fd2231bd
leejh6274 <thsou36@gmail.com>
2023-04-25 17:15:19 +0900

    조회수 증가를 다른 액션으로 분리

--- src/main/java/com/ljh/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/ljh/exam/demo/repository/ArticleRepository.java
@@ -28,5 +28,7 @@ public interface ArticleRepository {
 	public int getArticlesCount(int boardId, String searchKeywordTypeCode, String searchKeyword);
 
 	public int increaseHitCount(int id);
+
+	public int getArticleHitCount(int id);
 	
 }
